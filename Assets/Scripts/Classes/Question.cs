43ea7791d996a932338a800f850701626d98f872
thisellie <81550623+thisellie@users.noreply.github.com>
2024-09-04 14:13:40 +0800

    Repository cleanup
    Remove unused scripts and files

--- Assets/Scripts/Classes/Question.cs
+++
@@ -1,9 +0,0 @@
-[System.Serializable]
-public class Question
-{
-    public string question;
-    public string[] answers;
-    public int correctAnswerIndex;
-
-    public bool IsCorrect(int answerIndex) => answerIndex == correctAnswerIndex;
-}
