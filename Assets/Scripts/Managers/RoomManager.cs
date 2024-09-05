cd641201263738b32c18d6a2facfcc5b7172914a
thisellie <81550623+thisellie@users.noreply.github.com>
2024-09-05 19:16:30 +0800

    Update Room scene interface
    Add settings panel

--- Assets/Scripts/Managers/RoomManager.cs
+++ Assets/Scripts/Managers/RoomManager.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 using Photon.Pun;
 using TMPro;
 using System.Text.RegularExpressions;
-using System.Collections.Generic;
 
 public enum MinigameScenes
 {
@@ -20,26 +19,21 @@ public class RoomManager : MonoBehaviourPunCallbacks
 {
     [Header("UI")]
     public Button startButton;
-    public GameObject playerPrefab, countdownPanel, settingsPanel;
-    public TMP_Text roomText, countdownText;
+    public GameObject playerPrefab, countdownPanel, settingsPanel, roomDetails;
+    public TMP_Text roomText, countdownText, playerCountText, hostText, roomStatusText, nameText;
     public TMP_Dropdown topicDropdown;
+    [Space]
+    public Image[] statBars;
+    public TMP_Text[] statTexts, statusTexts;
 
     [Header("Settings")]
     public float startTime = 10.0f;
     public float minigameTime = 5f;
-    public float minX;
-    public float maxX;
-    public float minY;
-    public float maxY;
-    public Toggle[] dropChild;
-    public List<TMP_Dropdown.OptionData> test;
+    public Vector2[] positionBounds = new Vector2[2];
 
-#if UNITY_EDITOR
+    #if UNITY_EDITOR
     [Header("Debug")]
-    public MinigameScenes debugGame;
-    public TMP_Text debugText;
-    public TMP_Text hostText;
-    public TMP_Text roomStatusText;
+    public MinigameScenes debugGame = MinigameScenes.Random;
     #endif
 
     string[] minigames = new string[0];
@@ -48,56 +42,53 @@ public class RoomManager : MonoBehaviourPunCallbacks
 
     private void Start() 
     {
-        GetMinigames();
-        SpawnPlayers();
-        UpdateRoomDetails();
+        roomDetails.SetActive(false);
 
-        #if UNITY_EDITOR
-        ShowRoomDetails();
-        #endif
-
-        if (PhotonNetwork.IsMasterClient) {
+        if (PhotonNetwork.IsMasterClient) 
+        {
             topicDropdown.onValueChanged.AddListener(OnDropdownValueChanged);
+            PhotonNetwork.MasterClient.NickName = SaveManager.player.name;
         }
-        else {
+        else 
+        {
             topicDropdown.interactable = false;
             startButton.interactable = false;
         }
+
+        GetMinigames();
+        SpawnPlayers();
+        UpdatePlayerInterface();
+        UpdateRoomDetails();
     }
 
     #region Photon Callbacks
 
-    public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer) {
-        ShowRoomDetails();
-    }
-
-    public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer) {
-        ShowRoomDetails();
-    }
-
-    public override void OnMasterClientSwitched(Photon.Realtime.Player newMasterClient) {
-        HostCheck();
-    }
+    public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer) { UpdateRoomDetails(); }
+    public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer) { UpdateRoomDetails(); }
+    public override void OnMasterClientSwitched(Photon.Realtime.Player newMasterClient) { HostCheck(); }
 
     #endregion
 
     #region Countdown Function
 
-    public void TriggerCountdown() {
-        if (PhotonNetwork.IsMasterClient)
-            photonView.RPC("StartCountdown", RpcTarget.All);
+    public void TriggerCountdown() 
+    {
+        if (PhotonNetwork.IsMasterClient) photonView.RPC("StartCountdown", RpcTarget.All);
     }
 
     [PunRPC]
-    private void StartCountdown() {
+    private void StartCountdown() 
+    {
         countdownPanel.SetActive(true);
         settingsPanel.SetActive(false);
         StartCoroutine(Countdown());
     }
 
-    IEnumerator Countdown() {
+    IEnumerator Countdown() 
+    {
         int time = (int)startTime;
-        while (time > 0) {
+        while (time > 0) 
+        {
             countdownText.text = $"Game starts! Choosing a game in {time}";
             yield return new WaitForSeconds(1);
             time--;
@@ -110,7 +101,8 @@ public class RoomManager : MonoBehaviourPunCallbacks
 
     #region Minigame Initiation Functions
 
-    IEnumerator SelectMinigame() {
+    IEnumerator SelectMinigame() 
+    {
         int time = (int)minigameTime;
 
         #if UNITY_EDITOR
@@ -119,7 +111,8 @@ public class RoomManager : MonoBehaviourPunCallbacks
         string selectedGame = minigames[Random.Range(0, minigames.Length)];
         #endif
 
-        while (time > 0) {
+        while (time > 0) 
+        {
             countdownText.text = $"Chosen game is {Regex.Replace(selectedGame, "(?<=\\p{Ll})(?=\\p{Lu})", " ")}. Starting in {time}";
             yield return new WaitForSeconds(1);
             time--;
@@ -128,12 +121,14 @@ public class RoomManager : MonoBehaviourPunCallbacks
         RequestStartGame(selectedGame);
     }
 
-    public void RequestStartGame(string selectedGame) {
+    public void RequestStartGame(string selectedGame) 
+    {
         photonView.RPC("StartGame", RpcTarget.All, selectedGame);
     }
 
     [PunRPC]
-    public void StartGame(string selectedGame) {
+    public void StartGame(string selectedGame) 
+    {
         SetSelectedTopic();
         PhotonNetwork.DestroyPlayerObjects(PhotonNetwork.LocalPlayer);
         PhotonNetwork.LoadLevel(selectedGame);
@@ -143,13 +138,26 @@ public class RoomManager : MonoBehaviourPunCallbacks
 
     #region Room Synchronization Functions
 
-    void SpawnPlayers() {
-        Vector2 position = new(Random.Range(minX, maxX), Random.Range(minY, maxY));
+    void SpawnPlayers() 
+    {
+        Vector2 position = new(
+            Random.Range(positionBounds[0].x, positionBounds[1].x),
+            Random.Range(positionBounds[0].y, positionBounds[1].y)
+        );
+
         PhotonNetwork.Instantiate(playerPrefab.name, position, Quaternion.identity);
     }
 
-    private void UpdateRoomDetails() {
-        roomText.text = PhotonNetwork.CurrentRoom.Name;
+    private void UpdateRoomDetails() 
+    {
+        playerCountText.gameObject.SetActive(true);
+        hostText.gameObject.SetActive(true);
+        roomStatusText.gameObject.SetActive(true);
+        roomText.text = $"Code: {PhotonNetwork.CurrentRoom.Name}";
+        playerCountText.text = $"Player Count: {PhotonNetwork.CurrentRoom.PlayerCount} / {PhotonNetwork.CurrentRoom.MaxPlayers}";
+        hostText.text = $"Host: {PhotonNetwork.MasterClient.NickName}";
+        roomStatusText.text = $"Status: {(PhotonNetwork.CurrentRoom.IsOpen ? "Open" : "Closed")}";
+
         #if UNITY_EDITOR
         if (SaveManager.player == null) PhotonNetwork.LocalPlayer.NickName = $"Player {PhotonNetwork.LocalPlayer.ActorNumber}";
         else PhotonNetwork.LocalPlayer.NickName = $"{SaveManager.player.name}";
@@ -160,30 +168,36 @@ public class RoomManager : MonoBehaviourPunCallbacks
     }
 
     [PunRPC]
-    void RequestDropdownValue() {
+    void RequestDropdownValue() 
+    {
         photonView.RPC("UpdateDropdownValue", RpcTarget.Others, topicDropdown.value);
     }
 
     [PunRPC]
-    void UpdateDropdownValue(int value) {
+    void UpdateDropdownValue(int value) 
+    {
         topicDropdown.value = value;
     }
 
-    void OnDropdownValueChanged(int value) {
+    void OnDropdownValueChanged(int value) 
+    {
         photonView.RPC("UpdateDropdownValue", RpcTarget.All, value);
     }
 
-    public void SetSelectedTopic() {
-        if (PhotonNetwork.InRoom) {
+    public void SetSelectedTopic() 
+    {
+        if (PhotonNetwork.InRoom) 
+        {
             roomOptions["selectedTopic"] = GetTopic();
             PhotonNetwork.LocalPlayer.CustomProperties = roomOptions;
         }
-        else {
+        else 
+        {
             Debug.LogWarning("Not in a room. Cannot set custom property.");
         }
     }
-
-    string GetTopic() => topicDropdown.value switch {
+    string GetTopic() => topicDropdown.value switch 
+    {
         0 => "HOC",
         1 => "EOCS",
         2 => "NS",
@@ -195,24 +209,44 @@ public class RoomManager : MonoBehaviourPunCallbacks
 
     #region UI Functions
 
-    void ShowRoomDetails() {
-    #if UNITY_EDITOR
-            debugText.gameObject.SetActive(true);
-            hostText.gameObject.SetActive(true);
-            roomStatusText.gameObject.SetActive(true);
-            debugText.text = $"Player Count: {PhotonNetwork.CurrentRoom.PlayerCount} / {PhotonNetwork.CurrentRoom.MaxPlayers}";
-            hostText.text = $"Host: {PhotonNetwork.MasterClient.NickName}\n";
-            hostText.text += $"In Room: {PhotonNetwork.InRoom}";
-            roomStatusText.text = $"Room Status: {PhotonNetwork.CurrentRoom.IsOpen}";
-    #endif
+    public void UpdatePlayerInterface()
+    {
+        if (SaveManager.player.isNumberSystemUnlocked) statusTexts[0].transform.parent.gameObject.SetActive(false);
+        else statusTexts[0].text = "Locked";
+
+        if (SaveManager.player.isIntroProgrammingUnlocked) statusTexts[1].transform.parent.gameObject.SetActive(false);
+        else statusTexts[1].text = "Locked";
+
+        nameText.text = SaveManager.player.name;
+
+        float[] playerStats = {
+            SaveManager.player.computerHistory,
+            SaveManager.player.computerElements,
+            SaveManager.player.numberSystem,
+            SaveManager.player.introProgramming
+        };
+
+        for (int i = 0; i < playerStats.Length; i++)
+        {
+            statBars[i].fillAmount = playerStats[i];
+            statTexts[i].text = ((int)(playerStats[i] * 10000)).ToString();
+        }
     }
 
-    void HostCheck() {
-        if (PhotonNetwork.IsMasterClient) {
+    public void Toggle()
+    {
+        if (roomDetails != null) roomDetails.SetActive(!roomDetails.activeSelf);
+    }
+
+    void HostCheck() 
+    {
+        if (PhotonNetwork.IsMasterClient) 
+        {
             startButton.interactable = true;
             topicDropdown.interactable = true;
         }
-        else {
+        else 
+        {
             startButton.interactable = false;
             topicDropdown.interactable = false;
         }
