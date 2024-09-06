51f401a40d2d7cd7edb252febe8ef500c48d123e
kenparungao <kenparungao.024@gmail.com>
2024-09-06 11:28:52 +0800

    [UI Animation] Add LeanTween Initializer
    to fix animation error

---
+++ Assets/Scripts/Welcome/LeanTweenInitializer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeanTweenInitializer : MonoBehaviour
+{
+    void Start()
+    {
+        LeanTween.init(800); // Increase the number as needed
+    }
+}
