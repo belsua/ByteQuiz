a288e97f43707306e48e36c83fd26670dcb0ee10
thisellie <81550623+thisellie@users.noreply.github.com>
2024-09-06 19:27:31 +0800

    Update debug script
    Remove unused objects

--- Assets/Scripts/Managers/DebugManager.cs
+++ Assets/Scripts/Managers/DebugManager.cs
@@ -6,6 +6,20 @@ public class DebugManager : MonoBehaviour
 
     public void DebugIncrease(string topic)
     {
+        if (!SaveManager.player.isIntroProgrammingUnlocked && topic == "ITP")
+        {
+            cribManager.ShowMessage($"ITP is locked!");
+            cribManager.UpdatePlayerInterface();
+            return;
+        }
+
+        if (!SaveManager.player.isNumberSystemUnlocked && topic == "NS")
+        {
+            cribManager.ShowMessage($"NS is locked!");
+            cribManager.UpdatePlayerInterface();
+            return;
+        }
+
         SaveManager.player.IncreaseStat(topic, 0.10f);
         cribManager.ShowMessage($"Your {topic} stat increased!");
         cribManager.UpdatePlayerInterface();
